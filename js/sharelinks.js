51a213fe7dcaa424d6aec92c4b4e7df98bcc6c2c
Olivier Paroz <github@oparoz.com>
2015-06-22 18:12:17 +0200

    First working version

---
+++ js/sharelinks.js
@@ -0,0 +1,107 @@
+/* global OC, $, t */
+var ShareLinks = {};
+
+/**
+ * Adds buttons to the share dialogue
+ */
+ShareLinks.hijackShare = function () {
+	var target = OC.Share.showLink;
+	OC.Share.showLink = function () {
+		var r = target.apply(this, arguments);
+		var dropDownElement = $('#dropdown.drop.shareDropDown');
+		var linkSwitchButtonElement = $('#linkSwitchButton');
+		var urlElement = $('#linkText');
+
+		// TODO: Use a switch button
+		if (!linkSwitchButtonElement.length) {
+			linkSwitchButtonElement = $('<a/>').addClass('button').attr('id', 'linkSwitchButton');
+			$('#linkCheckbox+label').after(linkSwitchButtonElement);
+		}
+
+		if (dropDownElement.data('item-type') === "folder") {
+			ShareLinks.showFolderLinksButton(urlElement, linkSwitchButtonElement);
+		} else {
+			ShareLinks.showFileLinksButton(dropDownElement, urlElement, linkSwitchButtonElement);
+		}
+
+		$('#linkCheckbox').change(function () {
+			if (this.checked) {
+				linkSwitchButtonElement.show();
+			} else {
+				linkSwitchButtonElement.hide();
+			}
+		});
+
+		return r;
+	};
+};
+
+/**
+ * Shows button to change links for folders
+ *
+ * @param {*} urlElement
+ * @param {*} linkSwitchButtonElement
+ */
+ShareLinks.showFolderLinksButton = function (urlElement, linkSwitchButtonElement) {
+	linkSwitchButtonElement.text(t('sharelinks', 'Show Gallery link'));
+
+	linkSwitchButtonElement.toggle(function () {
+		$(this).text(t('sharelinks', "Show Files link"));
+		urlElement.val(urlElement.val().replace('index.php/s/',
+			'index.php/apps/galleryplus/s/'));
+	}, function () {
+		$(this).text(t('sharelinks', "Show Gallery link"));
+		urlElement.val(urlElement.val().replace('index.php/apps/galleryplus/s/',
+			'index.php/s/'));
+
+	});
+};
+
+/**
+ * Shows button to change links for files
+ *
+ * @param {*} dropDownElement
+ * @param {*} urlElement
+ * @param {*} linkSwitchButtonElement
+ */
+ShareLinks.showFileLinksButton = function (dropDownElement, urlElement, linkSwitchButtonElement) {
+	var filename = dropDownElement.parent().parent().data('file');
+	var extension = filename.substr(filename.lastIndexOf('.') + 1);
+
+	switch (extension.toLowerCase()) {
+		case 'gif':
+		case 'jpg':
+		case 'jpeg':
+		case 'png':
+		case 'bmp':
+		case 'svg':
+			var fileId = dropDownElement.data('item-source');
+			var tokenFilename = fileId + '.' + extension;
+			linkSwitchButtonElement.text(t('sharelinks', 'Show direct link'));
+
+			linkSwitchButtonElement.toggle(function () {
+				$(this).text(t('sharelinks', "Show Files link"));
+				urlElement.val(urlElement.val().replace('index.php/s/',
+						'index.php/apps/galleryplus/s/') + '/' + tokenFilename);
+			}, function () {
+				$(this).text(t('sharelinks', "Show direct link"));
+				urlElement.val(urlElement.val().replace('index.php/apps/galleryplus/s/',
+					'index.php/s/').replace('/' + tokenFilename, ''));
+			});
+			break;
+		default:
+			linkSwitchButtonElement.hide();
+	}
+};
+
+$(document).ready(function () {
+
+		if ($('#body-login').length > 0) {
+			return true; //deactivate on login page
+		}
+
+		if ($('#filesApp').val()) {
+			$('#fileList').on('updated', ShareLinks.hijackShare);
+		}
+	}
+);
